cd8c738507eabc589da046b387bdcc44ac0f3e55
yaitalam <yaitalam@charlotte.edu>
2025-03-21 09:27:10 -0400

    added env

---
+++ BeeModel/model.py
@@ -0,0 +1,88 @@
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+class BeeNet(nn.Module):
+    # Input:
+    #    x: Tensor<B, C * L, C * L>
+    #    comm: Tensor<B, B', D'>
+    #    C_prev: Tensor<B, D'>
+    # Output:
+    #    Tensor<B, A>
+    #    Tensor<B, D'>
+    # Takes as input the observation, the communication received by each bee,
+    # and the communication given by each bee at the previous timestep. Returns the batched Q vector and communication.
+    
+    
+    def __init__(self, inputdim):
+        super(BeeNet, self).__init__()
+        # self.eyes = self.Eyes(inputdim = inputdim ,render_style='bitmap')
+        
+        
+        # EYES
+        self.linear1 = nn.Linear(inputdim[1]*inputdim[2],128)
+        self.relu = nn.ReLU()
+        
+        # LSTM
+        
+        self.lstm = nn.LSTMCell(input_size = 128 , hiddin_size=128 , batch_first=True , dropout=0)
+        self.h_0 = nn.Parameter(torch.randn(inputdim[0],1, 128))
+        self.h_t = None
+        self.c_t = None
+        self.start = True
+        
+        # 
+        self.advatage = None
+        self.value = None
+    
+    def forward(self, state , comm , C_prev):
+        
+        # EYES
+        input_t = self.relu(self.linear1(state)) #Tensor<B, C * L, C * L> -> Tensor<B, 128>
+        
+        
+        input_t = input_t + C_prev
+        
+        #LSTM
+        if self.start:
+            self.lstm(input_t,)
+            
+        
+        
+        
+        
+        return
+    
+    # class Eyes(nn.Module):
+    #     def __init__(self, inputdim ,render_style="bitmap"):
+    #         super().__init__()
+            
+    #         if render_style == "Visual":
+    #             self.help = 1
+                
+    #         elif render_style == "bitmap":
+    #             self.linear1 = nn.Linear(inputdim[1]*inputdim[2],128)
+    #             self.relu = nn.ReLU()
+    #             self.linear2 = nn.Linear(128,64)
+        
+    #     def forward(self, state):
+    #         return self.relu(self.linear2(self.relu(self.linear1(state))))
+                
+            
+    
+    
+    
+class Model:
+    # __call__(x, comm, C_prev)
+    
+    def __call__(self, x, comm, C_prev):
+        return torch.tensor([1]), torch.tensor([1])
+    #end __call__
+    
+    def cuda(self):
+        return self
+    #end cuda
+#end Model
