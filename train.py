69e34f9d4a5a40da7de0943fff3535748ca3b7d7
Justin Smith <jsmit840@uncc.edu>
2025-04-23 17:19:48 -0400

    Removed exit

--- train.py
+++ train.py
@@ -119,8 +119,6 @@ def train(episodes, max_buffer, lr, gamma, epsilon, minibatch, target_update, nu
     optimizer = Adam(model.parameters(), lr = lr)
     for i in range(episodes):
         state = env.reset()
-        print(state[0].shape)
-        exit()
         terminated = False
         rewards = []
         states = [state]
